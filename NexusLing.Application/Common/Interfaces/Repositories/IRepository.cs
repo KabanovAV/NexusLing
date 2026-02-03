18798a273cac4fcbaef7cac73a3e7945b002824b
Кабанов Алексей <AVK.Kabanov@gmail.com>
2026-02-03 15:56:08 +0600

    Добавлен дженерик для работы с репозиторием

---
+++ NexusLing.Application/Common/Interfaces/Repositories/IRepository.cs
@@ -0,0 +1,16 @@
+﻿namespace NexusLing.Application.Common.Interfaces.Repositories
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetAsync(Guid id);
+        Task<T> AddAsync(T entity);
+        Task<IEnumerable<T>> AddRangeAsync(IEnumerable<T> entity);
+        void Update(T entity);
+        void UpdateRange(IEnumerable<T> entities);
+        void Delete(Guid id);
+        void DeleteRange(IEnumerable<Guid> ids);
+        void Delete(T entity);
+        void DeleteRange(IEnumerable<T> entities);
+    }
+}
