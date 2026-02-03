8296fce69c332be933997b8d93a1f98029ca5ea2
Кабанов Алексей <AVK.Kabanov@gmail.com>
2026-02-03 15:14:20 +0600

    Добавлен интерфейс IApplicationDbContext

---
+++ NexusLing.Application/Common/Interfaces/Database/IApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NexusLing.Domain.Entities;
+
+namespace NexusLing.Application.Common.Interfaces.Database
+{
+    public interface IApplicationDbContext
+    {
+        DbSet<User> Users { get; }
+    }
+}
