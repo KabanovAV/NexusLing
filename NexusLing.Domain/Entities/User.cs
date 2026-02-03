b262105c894356f9dd466c9fc98c35ab7ff87179
Кабанов Алексей <AVK.Kabanov@gmail.com>
2026-02-03 12:27:55 +0600

    Добавлена сущность пользователя

---
+++ NexusLing.Domain/Entities/User.cs
@@ -0,0 +1,12 @@
+﻿using NexusLing.Domain.Common;
+
+namespace NexusLing.Domain.Entities
+{
+    public class User : BaseAuditableEntity
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}
