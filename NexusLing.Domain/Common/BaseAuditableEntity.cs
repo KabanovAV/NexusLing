a82cd558e3480b663745283dc2c1fbc7e7b6934d
Кабанов Алексей <AVK.Kabanov@gmail.com>
2026-02-03 11:55:29 +0600

    Добавлен аудит класс для сущностей

---
+++ NexusLing.Domain/Common/BaseAuditableEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace NexusLing.Domain.Common
+{
+    public class BaseAuditableEntity : BaseEntity
+    {
+        public DateTime DateCreated { get; set; }
+        public DateTime DetaUpdated { get; set; }
+    }
+}
