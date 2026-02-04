d17f8e68f0a900915c7d8e8f05ada01f2493a666
Кабанов Алексей <AVK.Kabanov@gmail.com>
2026-02-04 14:29:28 +0600

    Добавлен класс подключения зависимостей

---
+++ NexusLing.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NexusLing.Application.Common.Interfaces.Database;
+using NexusLing.Infrastructure.Database;
+
+namespace NexusLing.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+            => services.AddDatabase(configuration);
+
+        public static IServiceCollection AddDatabase(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("Database");
+            services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(connectionString));
+            services.AddScoped<IApplicationDbContext>(options => options.GetRequiredService<ApplicationDbContext>());
+            return services;
+        }
+    }
+}
