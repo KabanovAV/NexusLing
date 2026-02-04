1544f434f6d74029f64a8e672462c657419c974d
Кабанов Алексей <AVK.Kabanov@gmail.com>
2026-02-04 12:23:36 +0600

    Внесены правки и добавлены коментарии в интерфейс IRepository. Описан класс Repository

--- NexusLing.Infrastructure/Repositories/Repository.cs
+++ NexusLing.Infrastructure/Repositories/Repository.cs
@@ -1,67 +1,144 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using NexusLing.Application.Common.Interfaces.Repositories;
 using NexusLing.Infrastructure.Database;
 
 namespace NexusLing.Infrastructure.Repositories
 {
-    public class Repository<T> : IRepository<T> where T : class
+    /// <summary>
+    /// Базовые операциии доступные у объектов в репозиториях
+    /// </summary>
+    /// <typeparam name="TEntity">DBDataContext или DBDocContext</typeparam>
+    /// <typeparam name="TKey">Объект, представляющий тип Id</typeparam>
+    public class Repository<TEntity, TKey> : IRepository<TEntity, TKey> where TEntity : class, new()
     {
         private ApplicationDbContext Context { get; set; }
-        private DbSet<T> DbSet => Context.Set<T>();
+        private DbSet<TEntity> DbSet => Context.Set<TEntity>();
 
         public Repository(ApplicationDbContext context)
         {
             Context = context;
         }
 
-        public Task<IEnumerable<T>> GetAllAsync()
+        /// <summary>
+        /// Полные данные объекта, включая данные других объектов через внешние связи
+        /// Может быть переопределён в конкретной реализации объекта данных
+        /// По умолчанию соответствует значению PlainData
+        /// </summary>
+        public virtual IQueryable<TEntity> Data => DbSet;
+        /// <summary>
+        /// Данные только одной таблицы.
+        /// Объекты (поля) связей с другими таблицами не заполнены (null)
+        /// </summary>
+        public IQueryable<TEntity> PlainData => DbSet;
+
+        /// <summary>
+        /// Получить один объект из набора данных
+        /// </summary>
+        /// <param name="id">Id объекта</param>
+        /// <returns></returns>
+        public virtual async Task<TEntity> GetAsync(TKey id)
+            => await DbSet.FindAsync(id);
+
+        /// <summary>
+        /// Добавить один объект в набор данных
+        /// </summary>
+        /// <param name="entity">Добавляемый объект</param>
+        /// <returns>Объект после добавления в БД</returns>
+        public virtual async Task<TEntity> AddAsync(TEntity entity)
         {
-            throw new NotImplementedException();
+            EntityEntry<TEntity> value = await DbSet.AddAsync(entity);
+            await Context.SaveChangesAsync();
+            return value.Entity;
         }
 
-        public Task<T> GetAsync(Guid id)
+        /// <summary>
+        /// Добавить коллекцию объектов в набор данных
+        /// </summary>
+        /// <param name="entities">Добавляемые объекты</param>
+        /// <returns>Объекты после добавления в БД</returns>
+        public virtual async Task<IEnumerable<TEntity>> AddRangeAsync(IEnumerable<TEntity> entities)
         {
-            throw new NotImplementedException();
+            List<TEntity> entityList = [.. entities];
+            if (entityList.Count == 0)
+                return [];
+            await DbSet.AddRangeAsync(entityList);
+            await Context.SaveChangesAsync();
+            return entityList;
         }
 
-        public Task<T> AddAsync(T entity)
+        /// <summary>
+        /// Изменить один объект в наборе данных
+        /// </summary>
+        /// <param name="entity">Изменяемый объект</param>
+        public virtual async Task Update(TEntity entity)
         {
-            throw new NotImplementedException();
+            DbSet.Update(entity);
+            await Context.SaveChangesAsync();
         }
 
-        public Task<IEnumerable<T>> AddRangeAsync(IEnumerable<T> entity)
+        /// <summary>
+        /// Изменить коллекцию объектов в наборе данных
+        /// </summary>
+        /// <param name="entities">Изменяемые объекты</param>
+        public virtual async Task UpdateRange(IEnumerable<TEntity> entities)
         {
-            throw new NotImplementedException();
+            List<TEntity> entityList = [.. entities];
+            if (entityList.Count == 0)
+                return;
+            DbSet.UpdateRange(entityList);
+            await Context.SaveChangesAsync();
         }
 
-        public void Update(T entity)
+        /// <summary>
+        /// Удалить один объект из набора данных
+        /// </summary>
+        /// <param name="id">Id объекта</param>
+        public virtual async Task Delete(TKey id)
         {
-            throw new NotImplementedException();
+            var entity = DbSet.Find(id);
+            DbSet.Remove(entity);
+            await Context.SaveChangesAsync();
         }
 
-        public void UpdateRange(IEnumerable<T> entities)
+        /// <summary>
+        /// Удалить коллекцию объектов из набора данных
+        /// </summary>
+        /// <param name="ids">Id объектов</param>
+        public virtual async Task DeleteRange(IEnumerable<TKey> ids)
         {
-            throw new NotImplementedException();
+            if (ids is ICollection<TEntity> idCollection && idCollection.Count > 0)
+            {
+                List<TEntity> entityList = await DbSet.Where(e => ids.Contains(EF.Property<TKey>(e, "Id")))
+                    .ToListAsync();
+                if (entityList.Count == 0)
+                    return;
+                DbSet.RemoveRange(entityList);
+                await Context.SaveChangesAsync();
+            }
         }
 
-        public void Delete(Guid id)
+        /// <summary>
+        /// Удалить один объект из набора данных
+        /// </summary>
+        /// <param name="entity">Удаляемый объект</param>
+        public virtual async Task Delete(TEntity entity)
         {
-            throw new NotImplementedException();
+            DbSet.Remove(entity);
+            await Context.SaveChangesAsync();
         }
 
-        public void Delete(T entity)
+        /// <summary>
+        /// Удалить коллекцию объектов из набора данных
+        /// </summary>
+        /// <param name="entities">Удаляемые объекты</param>
+        public virtual async Task DeleteRange(IEnumerable<TEntity> entities)
         {
-            throw new NotImplementedException();
-        }
-
-        public void DeleteRange(IEnumerable<Guid> ids)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void DeleteRange(IEnumerable<T> entities)
-        {
-            throw new NotImplementedException();
+            List<TEntity> entityList = [.. entities];
+            if (entityList.Count == 0)
+                return;
+            DbSet.RemoveRange(entityList);
+            await Context.SaveChangesAsync();
         }
     }
 }
