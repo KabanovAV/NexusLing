62d88732e3b2cba27bf2e30a8fc70247812425a4
Кабанов Алексей <AVK.Kabanov@gmail.com>
2026-02-04 10:05:17 +0600

    Добавлен класс ApplicationDbContext

---
+++ NexusLing.Infrastructure/Database/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NexusLing.Application.Common.Interfaces.Database;
+using NexusLing.Domain.Common;
+using NexusLing.Domain.Entities;
+
+namespace NexusLing.Infrastructure.Database
+{
+    public class ApplicationDbContext : DbContext, IApplicationDbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+            {
+                if (typeof(BaseEntity).IsAssignableFrom(entityType.ClrType))
+                {
+                    modelBuilder.Entity(entityType.ClrType)
+                        .Property("Id")
+                        .ValueGeneratedOnAdd()
+                        .IsRequired();
+                }
+            }
+        }
+    }
+}
