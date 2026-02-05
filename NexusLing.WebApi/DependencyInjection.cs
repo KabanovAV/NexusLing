e29bde4d10f7c7b004339f5ef04b9ff54c8bb0a1
Кабанов Алексей <AVK.Kabanov@gmail.com>
2026-02-05 09:32:22 +0600

    Добавлен класс для подключения зависимостей. В Program убраны комментарии и добавлено подключение сервиса

---
+++ NexusLing.WebApi/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using NexusLing.Infrastructure;
+
+namespace NexusLing.WebApi
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPresentation(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+            services.AddInfrastructure(configuration);
+            return services;
+        }
+    }
+}
