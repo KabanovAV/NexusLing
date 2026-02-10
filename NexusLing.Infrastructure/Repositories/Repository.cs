d4bdbc920b5b1690e69fa7ff1e921c7c14d5d004
Кабанов Алексей <AVK.Kabanov@gmail.com>
2026-02-10 14:07:23 +0600

    Добавлен класс Repository с реализацией интерфейса IRepository

---
+++ NexusLing.Infrastructure/Repositories/Repository.cs
@@ -0,0 +1,19 @@
+﻿using NexusLing.Domain.Common.Interfaces.Repositories;
+using NexusLing.Domain.Interfaces;
+using NexusLing.Infrastructure.Database;
+
+namespace NexusLing.Infrastructure.Repositories
+{
+    public class Repository : IRepository
+    {
+        /// <summary>
+        /// Пользователи
+        /// </summary>
+        public IUserRepository UserRepository { get; set; }
+
+        public Repository(ApplicationDbContext db)
+        {
+            UserRepository = new UserRepository(db);
+        }
+    }
+}
