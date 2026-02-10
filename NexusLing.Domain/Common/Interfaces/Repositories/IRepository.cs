931829367d2128773fa17010042c983b4a1582e4
Кабанов Алексей <AVK.Kabanov@gmail.com>
2026-02-10 12:14:54 +0600

    Добавлен интерфейс IRepository

---
+++ NexusLing.Domain/Common/Interfaces/Repositories/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using NexusLing.Domain.Interfaces;
+
+namespace NexusLing.Domain.Common.Interfaces.Repositories
+{
+    /// <summary>
+    /// Интерфейс к репозиториям данных
+    /// </summary>
+    public interface IRepository
+    {
+        /// <summary>
+        /// Пользователи
+        /// </summary>
+        IUserRepository UserRepository { get; }
+    }
+}
