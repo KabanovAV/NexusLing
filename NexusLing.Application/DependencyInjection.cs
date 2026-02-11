e9d8a48d3a3a9dab8bae0b20e876398a75aaaf8b
Кабанов Алексей <AVK.Kabanov@gmail.com>
2026-02-11 10:32:35 +0600

    Добавлено подключение сервиса UserService в зависимости

--- NexusLing.Application/DependencyInjection.cs
+++ NexusLing.Application/DependencyInjection.cs
@@ -1,10 +1,15 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using NexusLing.Application.Interfaces;
+using NexusLing.Application.Services;
 
 namespace NexusLing.Application
 {
     public static class DependencyInjection
     {
         public static IServiceCollection AddApplication(this IServiceCollection services)
-            => services;
+            => services.AddUserService();
+
+        public static IServiceCollection AddUserService(this IServiceCollection services)
+            => services.AddScoped<IUserService, UserService>();
     }
 }
