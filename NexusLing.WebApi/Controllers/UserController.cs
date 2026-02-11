113660a880f75526def8add8a1d847dcf5a4906d
Кабанов Алексей <AVK.Kabanov@gmail.com>
2026-02-11 16:51:57 +0600

    Обновлены методы контроллера, изменены типы возвращающего типа и параметры

--- NexusLing.WebApi/Controllers/UserController.cs
+++ NexusLing.WebApi/Controllers/UserController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using NexusLing.Application.DTOs;
 using NexusLing.Application.Interfaces;
 using NexusLing.Domain.Entities;
 
@@ -42,26 +43,26 @@ namespace NexusLing.WebApi.Controllers
             {
                 return NotFound(new { Message = $"Пользователь с id {id} не найден." });
             }
-            return Ok();
+            return Ok(user);
         }
 
         /// <summary>
         /// Добавить нового пользователя
         /// </summary>
-        /// <param name="user">Пользователь</param>
+        /// <param name="rUser">Пользователь</param>
         /// <returns>Возвращает нового пользователя</returns>
         /// <response code="200">Успешное выполнение запроса</response>
         /// <response code="400">Пустой обьект пользователя</response>
         [HttpPost]
         [ProducesResponseType(200)]
         [ProducesResponseType(400)]
-        public async Task<ActionResult<User>> AddUser(User user)
+        public async Task<ActionResult<User>> AddUser(RegisterUserDTO rUser)
         {
-            if (user == null)
+            if (rUser == null)
             {
                 return BadRequest(new { Message = "Данные для добавления пользователя пустые." });
             }
-            await _service.AddUserAsync(user);
+            var user = await _service.AddUserAsync(rUser);
             return CreatedAtAction(nameof(GetUser), new { id = user.Id }, user);
         }
 
@@ -69,32 +70,19 @@ namespace NexusLing.WebApi.Controllers
         /// Обновление данных пользователя
         /// </summary>
         /// <param name="id">Id пользователя</param>
-        /// <param name="user">Измененные данные пользователя</param>
+        /// <param name="uUser">Измененные данные пользователя</param>
         /// <response code="204">Успешное выполнение запроса</response>
-        /// <response code="400">Пустой обьект пользователя</response>
         /// <response code="404">Пользователь не найден</response>
         [HttpPut("{id:guid}")]
         [ProducesResponseType(204)]
-        [ProducesResponseType(400)]
         [ProducesResponseType(404)]
-        public async Task<IActionResult> UpdateUser(Guid id, User user)
+        public async Task<IActionResult> UpdateUser(Guid id, UpdateUserDTO uUser)
         {
-            if (user == null)
+            if (uUser == null)
             {
                 return BadRequest(new { Message = "Данные для добавления пользователя пустые." });
             }
-            var currentUser = await _service.GetUserAsync(id);
-            if (currentUser == null)
-            {
-                return NotFound(new { Message = $"Пользователь с id {id} не найден." });
-            }
-
-            currentUser.FirstName = user.FirstName;
-            currentUser.LastName = user.LastName;
-            currentUser.Login = user.Login;
-            currentUser.Password = user.Password;
-
-            await _service.UpdateUserAsync(currentUser);
+            await _service.UpdateUserAsync(id, uUser);
             return NoContent();
         }
 
@@ -103,18 +91,11 @@ namespace NexusLing.WebApi.Controllers
         /// </summary>
         /// <param name="id">Id пользователя</param>
         /// <response code="204">Успешное выполнение запроса</response>
-        /// <response code="404">Пользователь не найден</response>
         [HttpDelete("{id:guid}")]
         [ProducesResponseType(204)]
-        [ProducesResponseType(404)]
         public async Task<IActionResult> DeleteUser(Guid id)
         {
-            var user = await _service.GetUserAsync(id);
-            if (user == null)
-            {
-                return NotFound(new { Message = $"Пользователь с id {id} не найден." });
-            }
-            await _service.DeleteUserAsync(user);
+            await _service.DeleteUserAsync(id);
             return NoContent();
         }
     }
