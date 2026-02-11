c7d35c3fd29c4002036ad9cc6fd89f3ad294301a
Кабанов Алексей <AVK.Kabanov@gmail.com>
2026-02-11 09:55:56 +0600

    Добавлен класс UserService с реализацией интерфеса

---
+++ NexusLing.Application/Services/UserService.cs
@@ -0,0 +1,31 @@
+﻿using NexusLing.Application.Interfaces;
+using NexusLing.Domain.Entities;
+using NexusLing.Domain.Interfaces;
+
+namespace NexusLing.Application.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly IRepository _repository;
+
+        public UserService(IRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<IEnumerable<User>> GetAllUserAsync()
+            => await _repository.UserRepository.GetAllUserAsync();
+
+        public async Task<User> GetUserAsync(Guid id)
+            => await _repository.UserRepository.GetUserAsync(id);
+
+        public async Task<User> AddUserAsync(User user)
+            => await _repository.UserRepository.AddUserAsync(user);
+
+        public async Task UpdateUserAsync(User user)
+            => await _repository.UserRepository.UpdateUserAsync(user);
+
+        public async Task DeleteUserAsync(User user)
+            => await _repository.UserRepository.DeleteUserAsync(user);
+    }
+}
