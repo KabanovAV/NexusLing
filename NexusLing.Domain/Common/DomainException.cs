47bede8059d2c314eb10c1a5f319852deaa484a2
Кабанов Алексей <AVK.Kabanov@gmail.com>
2026-02-16 12:03:46 +0600

    Добавлен класс доменного исключения

---
+++ NexusLing.Domain/Common/DomainException.cs
@@ -0,0 +1,8 @@
+﻿namespace NexusLing.Domain.Common
+{
+    public class DomainException : Exception
+    {
+        public DomainException(string message) 
+            : base(message) { }
+    }
+}
