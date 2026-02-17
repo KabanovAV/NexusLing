2aa9292240653b153f8c85731e24cad2a73e4e3a
Кабанов Алексей <AVK.Kabanov@gmail.com>
2026-02-17 11:12:41 +0600

    Добавлен middleware для обработки исключений

---
+++ NexusLing.WebApi/Middlewares/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,32 @@
+﻿using NexusLing.Domain.Exceptions;
+
+namespace NexusLing.WebApi.Middlewares
+{
+    public class ExceptionHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionHandlingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (NotFoundException ex)
+            {
+                context.Response.StatusCode = StatusCodes.Status404NotFound;
+                await context.Response.WriteAsJsonAsync(new { error = ex.Message });
+            }
+            catch (DomainException ex)
+            {
+                context.Response.StatusCode = StatusCodes.Status400BadRequest;
+                await context.Response.WriteAsJsonAsync(new { error = ex.Message });
+            }            
+        }
+    }
+}
--- NexusLing.WebApi/Program.cs
+++ NexusLing.WebApi/Program.cs
@@ -1,4 +1,5 @@
 using NexusLing.WebApi;
+using NexusLing.WebApi.Middlewares;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -21,6 +22,8 @@ app.UseSerilogRequestLogging();
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseMiddleware<ExceptionHandlingMiddleware>();
 
 app.MapControllers();
 
