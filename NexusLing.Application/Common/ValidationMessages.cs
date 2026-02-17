6fe1d93795498ce4f5360550f48d73e0ee5d677b
Кабанов Алексей <AVK.Kabanov@gmail.com>
2026-02-17 17:15:39 +0600

    Добавлен: FluentValidation; класс для валидации регистрации пользователя; статистический класс для повторяющихся сообщений

---
+++ NexusLing.Application/Common/ValidationMessages.cs
@@ -0,0 +1,8 @@
+﻿namespace NexusLing.Application.Common
+{
+    public static class ValidationMessages
+    {
+        public const string Required = "Поле {0} обязательно для заполнения";
+        public const string Length = "Поле {0} должно быть от {1} до {2} символов";
+    }
+}
---
+++ NexusLing.Application/Validators/RegisterUserValidator.cs
@@ -0,0 +1,36 @@
+﻿using FluentValidation;
+using NexusLing.Application.Common;
+using NexusLing.Application.DTOs;
+
+namespace NexusLing.Application.Validators
+{
+    public class RegisterUserValidator : AbstractValidator<RegisterUserDTO>
+    {
+        public RegisterUserValidator()
+        {
+            RuleFor(u => u.FirstName)
+                .NotEmpty().WithMessage(x => string.Format(ValidationMessages.Required, "Имя"))
+                .Matches(@"^[a-zA-Zа-яА-Я\s\-]+$").WithMessage("Имя может содержать только буквы, пробелы и дефис");
+
+            RuleFor(u => u.LastName)
+                .NotEmpty().WithMessage(x => string.Format(ValidationMessages.Required, "Фамилия"))
+                .Matches(@"^[a-zA-Zа-яА-Я\s\-]+$").WithMessage("Фамилия может содержать только буквы, пробелы и дефис");
+
+            RuleFor(u => u.Login)
+                .NotEmpty().WithMessage(x => string.Format(ValidationMessages.Required, "Логин"))
+                .Length(3, 64).WithMessage(x => string.Format(ValidationMessages.Length, "Логин", 3, 64));
+
+            RuleFor(u => u.Password)
+                .NotEmpty().WithMessage(x => string.Format(ValidationMessages.Required, "Пароль"))
+                .Length(8, 64).WithMessage(x => string.Format(ValidationMessages.Length, "Пароль", 8, 64))
+                .Must(u => u.Any(char.IsUpper)).WithMessage("Пароль должен содержать заглавную букву")
+                .Must(u => u.Any(char.IsLower)).WithMessage("Пароль должен содержать строчную букву")
+                .Must(u => u.Any(char.IsDigit)).WithMessage("Пароль должен содержать цифру")
+                .Must(u => u.Any(ch => !char.IsLetterOrDigit(ch))).WithMessage("Пароль должен содержать специальный символ");
+
+            RuleFor(u => u.ConfirmPassword)
+                .NotEmpty().WithMessage("Подтверждение пароля обязательно")
+                .Equal(u => u.Password).WithMessage("Пароли не совпадают");
+        }
+    }
+}
