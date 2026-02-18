fddcf87db7de9a908e59ef8def042777472854fc
Кабанов Алексей <AVK.Kabanov@gmail.com>
2026-02-18 15:24:01 +0600

    Внесены правки в отображении исключений. Добавлен логирование исключений

--- NexusLing.WebApi/Middlewares/ExceptionHandlingMiddleware.cs
+++ NexusLing.WebApi/Middlewares/ExceptionHandlingMiddleware.cs
@@ -1,6 +1,6 @@
 ﻿using NexusLing.Application.Common.Exceptions;
 using NexusLing.Domain.Exceptions;
-using System.Text.Json;
+using Serilog;
 
 namespace NexusLing.WebApi.Middlewares
 {
@@ -21,28 +21,40 @@ namespace NexusLing.WebApi.Middlewares
             }
             catch (NotFoundException ex)
             {
+                Log.Warning(ex, "Ресурс не найден: {Path}", context.Request.Path);
                 context.Response.StatusCode = StatusCodes.Status404NotFound;
-                await context.Response.WriteAsJsonAsync(new { message = ex.Message });
+                await context.Response.WriteAsJsonAsync(new
+                {
+                    status = 404,
+                    title = "Ресурс не найден",
+                    message = ex.Message,
+                    path = context.Request.Path
+                });
             }
             catch (DomainException ex)
             {
+                Log.Warning(ex, "Ошибка домена: {Path}", context.Request.Path);
                 context.Response.StatusCode = StatusCodes.Status400BadRequest;
-                await context.Response.WriteAsJsonAsync(new { message = ex.Message });
+                await context.Response.WriteAsJsonAsync(new
+                {
+                    status = 400,
+                    title = "Ошибка бизнес-логики",
+                    message = ex.Message,
+                    path = context.Request.Path
+                });
             }
             catch (ValidatorException ex)
             {
+                Log.Warning(ex, "Ошибка валидации: {Path} метод {Method}", context.Request.Path, context.Request.Method);
                 context.Response.StatusCode = StatusCodes.Status400BadRequest;
-                context.Response.ContentType = "application/json";
-
-                var response = new
+                await context.Response.WriteAsJsonAsync(new
                 {
                     status = 400,
                     title = "Ошибка валидации",
                     message = ex.Message,
-                    errors = ex.Errors
-                };
-
-                await context.Response.WriteAsJsonAsync(JsonSerializer.Serialize(response));
+                    errors = ex.Errors,
+                    path = context.Request.Path
+                });
             }
         }
     }
