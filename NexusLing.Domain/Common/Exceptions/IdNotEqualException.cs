3815103b61f39d4b6608411994390700587e2a2a
Кабанов Алексей <AVK.Kabanov@gmail.com>
2026-02-18 16:35:47 +0600

    Добавлено исключение на несовпадение идентификаторов

---
+++ NexusLing.Domain/Common/Exceptions/IdNotEqualException.cs
@@ -0,0 +1,10 @@
+﻿using NexusLing.Domain.Exceptions;
+
+namespace NexusLing.Domain.Common.Exceptions
+{
+    public class IdNotEqualException : DomainException
+    {
+        public IdNotEqualException(object expectedId, object actualId)
+            : base($"Несовпадение идентификаторов: ожидался '{expectedId}', получен '{actualId}'") { }
+    }
+}
