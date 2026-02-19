7ec47a9451c95852655cfc10300bd18396694d46
Кабанов Алексей <AVK.Kabanov@gmail.com>
2026-02-19 15:46:16 +0600

    Убрана проверка валидации пароля

--- NexusLing.Application/Validators/UpdateUserValidator.cs
+++ NexusLing.Application/Validators/UpdateUserValidator.cs
@@ -1,5 +1,4 @@
 ﻿using FluentValidation;
-using NexusLing.Application.Common;
 using NexusLing.Application.DTOs;
 using NexusLing.Domain.Interfaces;
 
@@ -10,14 +9,6 @@ namespace NexusLing.Application.Validators
         public UpdateUserValidator(IRepository repository)
         {
             Include(new UserBaseValidator<UpdateUserDTO>(repository));
-
-            RuleFor(u => u.Password)
-                .NotEmpty().WithMessage(x => string.Format(ValidationMessages.Required, "Пароль"))
-                .Length(8, 64).WithMessage(x => string.Format(ValidationMessages.Length, "Пароль", 8, 64))
-                .Must(u => u.Any(char.IsUpper)).WithMessage("Пароль должен содержать заглавную букву")
-                .Must(u => u.Any(char.IsLower)).WithMessage("Пароль должен содержать строчную букву")
-                .Must(u => u.Any(char.IsDigit)).WithMessage("Пароль должен содержать цифру")
-                .Must(u => u.Any(ch => !char.IsLetterOrDigit(ch))).WithMessage("Пароль должен содержать специальный символ");
         }
     }
 }
