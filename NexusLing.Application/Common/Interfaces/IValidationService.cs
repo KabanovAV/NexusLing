b2bfd627415bbc187d31abce1c421f703e02ab91
Кабанов Алексей <AVK.Kabanov@gmail.com>
2026-02-19 10:18:52 +0600

    Добавлен интерфейс для валидации

---
+++ NexusLing.Application/Common/Interfaces/IValidationService.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation.Results;
+
+namespace NexusLing.Application.Common.Interfaces
+{
+    internal interface IValidationService
+    {
+        /// <summary>
+        /// Валидирует объект и возвращает результат
+        /// </summary>
+        Task<ValidationResult> ValidateAsync<T>(T instance);
+
+        /// <summary>
+        /// Валидирует объект и выбрасывает исключение при ошибках
+        /// </summary>
+        Task ValidateAndThrowAsync<T>(T instance);
+    }
+}
