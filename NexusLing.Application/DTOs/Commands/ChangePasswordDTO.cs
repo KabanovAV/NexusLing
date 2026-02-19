62b11428cba5e7adb5e8996d386553d18f6c3e6c
Кабанов Алексей <AVK.Kabanov@gmail.com>
2026-02-19 16:01:33 +0600

    Изменено название  DTO с ChangePasswordUserDTO на ChangePasswordDTO

---
+++ NexusLing.Application/DTOs/Commands/ChangePasswordDTO.cs
@@ -0,0 +1,4 @@
+﻿namespace NexusLing.Application.DTOs
+{
+    public record ChangePasswordDTO(Guid Id, string Password, string ConfirmPassword);
+}
--- NexusLing.Application/DTOs/Commands/ChangePasswordUserDTO.cs
+++
@@ -1,4 +0,0 @@
-﻿namespace NexusLing.Application.DTOs
-{
-    public record ChangePasswordUserDTO(Guid Id, string Password, string ConfirmPassword);
-}
