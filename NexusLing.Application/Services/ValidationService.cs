32dc2d42220e17e06484f3e171fec71ab9ad0ad3
Кабанов Алексей <AVK.Kabanov@gmail.com>
2026-02-19 10:35:01 +0600

    Добавлен сервис валидации обьектов DTO

---
+++ NexusLing.Application/Services/ValidationService.cs
@@ -0,0 +1,43 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using Microsoft.Extensions.DependencyInjection;
+using NexusLing.Application.Common.Exceptions;
+using NexusLing.Application.Common.Interfaces;
+using NexusLing.Application.Validators;
+
+namespace NexusLing.Application.Services
+{
+    public class ValidationService : IValidationService
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public ValidationService(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public async Task ValidateAndThrowAsync<T>(T instance)
+        {
+            var validator = _serviceProvider.GetService<IValidator<T>>();
+            if (validator == null)
+                return;
+
+            var validatorResult = await validator.ValidateAsync(instance);
+            if (!validatorResult.IsValid)
+            {
+                var errorResponse = validatorResult.Errors
+                    .GroupBy(e => e.PropertyName)
+                    .ToDictionary(g => g.Key, g => g.Select(e => e.ErrorMessage).ToArray());
+                throw new ValidatorException(errorResponse);
+            }
+        }
+
+        public async Task<ValidationResult> ValidateAsync<T>(T instance)
+        {
+            var validator = _serviceProvider.GetService<IValidator<T>>();
+            if (validator == null)
+                return new ValidationResult();
+            return await validator.ValidateAsync(instance);
+        }
+    }
+}
