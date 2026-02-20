87102d72bf0afa99f7ff1992282d9175488214bc
Кабанов Алексей <AVK.Kabanov@gmail.com>
2026-02-20 13:48:40 +0600

    Поля изменены на nullable

--- NexusLing.Application/DTOs/Bases/UserBaseDTO.cs
+++ NexusLing.Application/DTOs/Bases/UserBaseDTO.cs
@@ -1,4 +1,4 @@
 ﻿namespace NexusLing.Application.Bases
 {
-    public record UserBaseDTO(string FirstName, string LastName, string Login);
+    public record UserBaseDTO(string? FirstName, string? LastName, string? Login);
 }
