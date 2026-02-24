d72104640467d924925cb583b6d5b45f6da89ace
Кабанов Алексей <AVK.Kabanov@gmail.com>
2026-02-24 15:31:24 +0600

    Добавлен валидатор логина

---
+++ NexusLing.Application/Validators/LoginValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using NexusLing.Application.Common;
+using NexusLing.Application.DTOs;
+
+namespace NexusLing.Application.Validators
+{
+    public class LoginValidator : AbstractValidator<LoginDTO>
+    {
+        public LoginValidator()
+        {
+            RuleFor(u => u.Login)
+                    .NotEmpty().WithMessage(x => string.Format(ValidationMessages.Required, "Логин"))
+                    .Length(3, 64).WithMessage(x => string.Format(ValidationMessages.LengthFromTo, "Логин", 3, 64));
+
+            RuleFor(u => u.Password)
+                .NotEmpty().WithMessage(x => string.Format(ValidationMessages.Required, "Пароль"))
+                .Length(8, 64).WithMessage(x => string.Format(ValidationMessages.Length, "Пароль", 8));
+        }
+    }
+}
