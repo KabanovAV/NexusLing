abfc786f26b8f9d2d20968f1d84c3c8657bcf0ba
Кабанов Алексей <AVK.Kabanov@gmail.com>
2026-02-25 11:41:36 +0600

    Добавлен провайдер с работой с токенами. Расписан метод логирования

--- NexusLing.Application/Services/AuthService.cs
+++ NexusLing.Application/Services/AuthService.cs
@@ -1,23 +1,31 @@
 ﻿using NexusLing.Application.Common.Interfaces;
 using NexusLing.Application.DTOs;
 using NexusLing.Application.Interfaces;
+using NexusLing.Domain.Exceptions;
+using NexusLing.Domain.ValueObjects;
 
 namespace NexusLing.Application.Services
 {
     public class AuthService : IAuthService
     {
         private readonly IUserService _service;
+        private readonly IJwtProvider _jwtProvider;
         private readonly IValidationService _validationService;
 
-        public AuthService(IUserService service, IValidationService validationService)
+        public AuthService(IUserService service, IJwtProvider jwtProvider, IValidationService validationService)
         {
             _service = service;
+            _jwtProvider = jwtProvider;
             _validationService = validationService;
         }
 
-        public Task<string> LoginAsync(LoginDTO login)
+        public async Task<string> LoginAsync(LoginDTO request)
         {
-            throw new NotImplementedException();
+            await _validationService.ValidateAndThrowAsync(request);
+            var login = Login.Create(request.Login);
+            var user = await _service.GetUserByLoginAsync(login.Value) ?? throw new NotFoundException("User", "логин", login.Value);
+            string token = _jwtProvider.Generate(user);
+            return token;
         }
     }
 }
