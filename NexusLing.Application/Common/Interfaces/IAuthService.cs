4a812e637a66ec82809586f12aea6cc0971e8046
Кабанов Алексей <AVK.Kabanov@gmail.com>
2026-02-25 10:40:45 +0600

    Добавлен интерфейс и сервис для авторизации пользоваетля

---
+++ NexusLing.Application/Common/Interfaces/IAuthService.cs
@@ -0,0 +1,9 @@
+﻿using NexusLing.Application.DTOs;
+
+namespace NexusLing.Application.Common.Interfaces
+{
+    public interface IAuthService
+    {
+        Task<string> LoginAsync(LoginDTO login);
+    }
+}
---
+++ NexusLing.Application/Services/AuthService.cs
@@ -0,0 +1,23 @@
+﻿using NexusLing.Application.Common.Interfaces;
+using NexusLing.Application.DTOs;
+using NexusLing.Domain.Interfaces;
+
+namespace NexusLing.Application.Services
+{
+    public class AuthService : IAuthService
+    {
+        private readonly IRepository _repository;
+        private readonly IValidationService _validationService;
+
+        public AuthService(IRepository repository, IValidationService validationService)
+        {
+            _repository = repository;
+            _validationService = validationService;
+        }
+
+        public Task<string> LoginAsync(LoginDTO login)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
