6194e995a58cefc5faa66fdabe25024978da3122
Кабанов Алексей <AVK.Kabanov@gmail.com>
2026-02-25 11:04:47 +0600

    Добавлено исключение на не найденый логин

---
+++ NexusLing.Domain/Common/Exceptions/NotFoundLoginException.cs
@@ -0,0 +1,8 @@
+﻿namespace NexusLing.Domain.Exceptions
+{
+    public class NotFoundLoginException : DomainException
+    {
+        public NotFoundLoginException(string entityName, string login)
+            : base($"Сущность '{entityName}' с логином '{login}' не найдена") { }
+    }
+}
