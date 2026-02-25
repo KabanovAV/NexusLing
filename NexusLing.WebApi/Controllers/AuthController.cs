acf3a84ed895ef040631566304af2f610b3b4d82
Кабанов Алексей <AVK.Kabanov@gmail.com>
2026-02-25 11:53:45 +0600

    Добавлен контроллер авторизации пользователя

---
+++ NexusLing.WebApi/Controllers/AuthController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NexusLing.Application.DTOs;
+using NexusLing.Application.Interfaces;
+
+namespace NexusLing.WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : Controller
+    {
+        private readonly IAuthService _service;
+
+        public AuthController(IAuthService service)
+        {
+            _service = service;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login([FromBody] LoginDTO login)
+        {
+            var tokenResult = await _service.LoginAsync(login);
+            return Ok(tokenResult);
+        }
+    }
+}
