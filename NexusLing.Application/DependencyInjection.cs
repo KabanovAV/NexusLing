d022e2c02ff60ae3a8b055e23ddee244bb2e2c4f
Кабанов Алексей <AVK.Kabanov@gmail.com>
2026-02-25 11:46:20 +0600

    Добавлен DI на сервис авторизации

--- NexusLing.Application/DependencyInjection.cs
+++ NexusLing.Application/DependencyInjection.cs
@@ -10,16 +10,19 @@ namespace NexusLing.Application
     public static class DependencyInjection
     {
         public static IServiceCollection AddApplication(this IServiceCollection services)
-            => services.AddUserService().AddValidationService();
+            => services.AddUserService().AddAuthService().AddValidationService();
 
         public static IServiceCollection AddUserService(this IServiceCollection services)
             => services.AddScoped<IUserService, UserService>();
+
+        public static IServiceCollection AddAuthService(this IServiceCollection services)
+            => services.AddScoped<IAuthService, AuthService>();
 
         public static IServiceCollection AddValidationService(this IServiceCollection services)
         {
             services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
             services.AddScoped<IValidationService, ValidationService>();
             return services;
-        }            
+        }
     }
 }
