29b9992c99f8480b1462dde997ec874ab2ed7603
Кабанов Алексей <AVK.Kabanov@gmail.com>
2026-02-26 15:52:03 +0600

    В класс добавлены свойства, конструктор и методы результата

--- NexusLing.Application/Common/Result/Error.cs
+++ NexusLing.Application/Common/Result/Error.cs
@@ -1,7 +1,31 @@
-﻿namespace NexusLing.Application.Common.Result
+﻿namespace NexusLing.Application.Result
 {
     public record Error
     {
-        
+        public string Code { get; }
+        public string Description { get; }
+        public ErrorType Type { get; }
+
+        public Error(string code, string description, ErrorType type)
+        {
+            Code = code;
+            Description = description;
+            Type = type;
+        }
+
+        public static readonly Error None = new(string.Empty, string.Empty, ErrorType.Failure);
+        public static readonly Error NullValue = new("General.Null", "Было предоставлено нулевое значение.", ErrorType.Failure);
+
+        public static Error Failure(string code, string description)
+            => new(code, description, ErrorType.Failure);
+
+        public static Error NotFound(string code, string description)
+            => new(code, description, ErrorType.NotFound);
+
+        public static Error Problem(string code, string description)
+            => new(code, description, ErrorType.Problem);
+
+        public static Error Conflict(string code, string description)
+            => new(code, description, ErrorType.Conflict);
     }
 }
