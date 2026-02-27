d9a89a094857aeda1f4c1776704102db690f4f76
Кабанов Алексей <AVK.Kabanov@gmail.com>
2026-02-27 11:50:25 +0600

    Изменен возвращающийся тип

--- NexusLing.Application/Common/Interfaces/IAuthService.cs
+++ NexusLing.Application/Common/Interfaces/IAuthService.cs
@@ -1,9 +1,10 @@
-﻿using NexusLing.Application.DTOs;
+﻿using NexusLing.Application.Common;
+using NexusLing.Application.DTOs;
 
 namespace NexusLing.Application.Interfaces
 {
     public interface IAuthService
     {
-        Task<string> LoginAsync(LoginDTO request);
+        Task<Result<string>> LoginAsync(LoginDTO request);
     }
 }
--- NexusLing.Application/Services/AuthService.cs
+++ NexusLing.Application/Services/AuthService.cs
@@ -1,7 +1,7 @@
-﻿using NexusLing.Application.Common.Interfaces;
+﻿using NexusLing.Application.Common;
+using NexusLing.Application.Common.Interfaces;
 using NexusLing.Application.DTOs;
 using NexusLing.Application.Interfaces;
-using NexusLing.Domain.Exceptions;
 using NexusLing.Domain.ValueObjects;
 
 namespace NexusLing.Application.Services
@@ -19,13 +19,19 @@ namespace NexusLing.Application.Services
             _validationService = validationService;
         }
 
-        public async Task<string> LoginAsync(LoginDTO request)
+        public async Task<Result<string>> LoginAsync(LoginDTO request)
         {
-            await _validationService.ValidateAndThrowAsync(request);
-            var login = Login.Create(request.Login);
-            var user = await _service.GetUserByLoginAsync(login.Value) ?? throw new NotFoundException("User", "логин", login.Value);
-            string token = _jwtProvider.Generate(user);
-            return token;
+            var result = await _validationService.ValidateAsync(request);
+            if (result.IsSuccess)
+            {
+                var login = Login.Create(request.Login);
+                var userResult = await _service.GetUserByLoginAsync(login.Value);
+                if (userResult.IsFailure)
+                    Result.Failure<string>(userResult.Error);
+                string token = _jwtProvider.Generate(userResult.Value);
+                return token;
+            }
+            return Result.Failure<string>(result.Error);
         }
     }
 }
